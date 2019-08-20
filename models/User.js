dd3f94b02954aab59ec1b35319d528fca76b39df
daltonhart <daltonvaren@gmail.com>
2019-08-20 15:30:53 -0700

    adds additional match to check email format in user model

--- models/User.js
+++ models/User.js
@@ -15,7 +15,8 @@ module.exports = mongoose.model(
     email: {
       type: String,
       required: true,
-      unique: true
+      unique: true,
+      match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
     password: {
       type: String,
