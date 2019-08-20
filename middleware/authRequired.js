4c4552fda53e44f2a7c00912e117f90431d023e6
daltonhart <daltonvaren@gmail.com>
2019-08-20 15:18:42 -0700

    adds reformat to error response with authrequired

--- middleware/authRequired.js
+++ middleware/authRequired.js
@@ -1,6 +1,8 @@
+const { errorResponse } = require("../controllers/responses");
+
 module.exports = (req, res, next) => {
   if (!req.session.currentUser)
-    return res.status(401).json({
+    return errorResponse(res, {
       status: 401,
       message: "Unauthorized. Please login and try again."
     });
